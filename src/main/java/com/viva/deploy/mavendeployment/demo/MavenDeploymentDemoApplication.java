c3911b41cb685c6489831d29d7bbd9263b9006fc
unknown <Administrator@MCGD-TRN065.COMVIVA.COM>
2020-02-17 16:50:37 +0530

    Collab Commit One

---
+++ src/main/java/com/viva/deploy/mavendeployment/demo/MavenDeploymentDemoApplication.java
@@ -0,0 +1,13 @@
+package com.viva.deploy.mavendeployment.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenDeploymentDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenDeploymentDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viva/deploy/mavendeployment/demo/MavenDeploymentDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viva.deploy.mavendeployment.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenDeploymentDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
